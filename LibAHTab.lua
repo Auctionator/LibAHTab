4ea85391c1681f85080ae874a62a5eabc9c3cee2
plusmouse <plusmouse@protonmail.com>
2022-11-01 08:13:32 +0000

    Error messages for invalid arguments

--- LibAHTab.lua
+++ LibAHTab.lua
@@ -5,17 +5,18 @@ if not lib or lib.internalState then return end
 local MIN_TAB_WIDTH = 70
 local TAB_PADDING = 20
 
+local tabIDErrorMessage = "tabID should be a string"
+
 function lib:DoesIDExist(tabID)
+  assert(type(tabID) == "string", tabIDErrorMessage)
   return lib.internalState and lib.internalState.usedIDs[tabID] ~= nil
 end
 
 function lib:CreateTab(tabID, attachedFrame, displayText)
-  if not AuctionHouseFrame then
-    error("Wait for the AH to open before creating your tab")
-  end
-  if type(tabID) ~= "string" then
-    error("tabID should be a string")
-  end
+  assert(AuctionHouseFrame, "Wait for the AH to open before creating your tab")
+  assert(type(tabID) == "string", tabIDErrorMessage)
+  assert(type(attachedFrame) == "table" and attachedFrame.IsObjectType and attachedFrame:IsObjectType("Frame"), "attachedFrame should be a frame")
+  assert(type(displayText) == "string", "displayText should be a string")
 
   if not lib.internalState then
     lib.internalState = {
@@ -68,10 +69,12 @@ function lib:CreateTab(tabID, attachedFrame, displayText)
 end
 
 function lib:GetButton(tabID)
+  assert(type(tabID) == "string", tabIDErrorMessage)
   return lib.internalState.usedIDs[tabID]
 end
 
 function lib:SetSelected(tabID)
+  assert(type(tabID) == "string", tabIDErrorMessage)
   if lib.internalState == nil or not lib:DoesIDExist(tabID) then
     error("Tab doesn't exist")
   end
